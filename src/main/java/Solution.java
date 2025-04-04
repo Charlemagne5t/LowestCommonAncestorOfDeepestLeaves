2b46ece2de200a35695f7c3407ff248781aa6725
alek <wait82985@gmail.com>
2025-04-04 15:01:21 +0500

    problem solved more clear

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,61 +2,51 @@ import java.util.*;
 
 public class Solution {
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        Map<Integer, TreeNode> map = new HashMap<>();
-        Queue<Path> q = new LinkedList<>();
-        q.offer(new Path(root, root.val + ""));
-        List<Path> currentLevel = new ArrayList<>();
-        while (!q.isEmpty()) {
+        Deque<TreeNode> q =new ArrayDeque<>();
+        q.offer(root);
+        Set<Integer> deepest = new HashSet<>();
+        int level = 0;
+        while(!q.isEmpty()) {
             int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                Path p = q.poll();
-                currentLevel.add(p);
-                map.put(p.node.val, p.node);
-                if (p.node.left != null) {
-                    q.offer(new Path(p.node.left, p.path + "-" + p.node.left.val));
+            for(int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                deepest.add(cur.val);
+                if(cur.left != null){
+                    q.offer(cur.left);
                 }
-                if (p.node.right != null) {
-                    q.offer(new Path(p.node.right, p.path + "-" + p.node.right.val));
+                if(cur.right != null){
+                    q.offer(cur.right);
                 }
             }
-            if (!q.isEmpty()) {
-                currentLevel.clear();
+            if(!q.isEmpty()){
+                deepest = new HashSet<>();
+                level++;
             }
-        }
-        if (currentLevel.size() == 1) {
-            return currentLevel.get(0).node;
+
         }
 
-        //currentLevel.forEach(x -> System.out.println(x.path));
-        String[][] paths = new String[currentLevel.size()][];
 
-        for (int i = 0; i < currentLevel.size(); i++) {
-            paths[i] = currentLevel.get(i).path.split("-");
-        }
-        TreeNode lca = root;
-        for (int j = 0; j < paths[0].length; j++) {
-            for (int i = 1; i < paths.length; i++) {
-                if (!paths[i][j].equals(paths[0][j])) {
-                    return lca;
-                }
-            }
-            lca = map.get(Integer.parseInt(paths[0][j]));
-        }
-        return null;
+
+        return dfs(root, deepest);
     }
-}
+    TreeNode dfs(TreeNode root, Set<Integer> set){
+        if(root == null){
+            return null;
+        }
+        if(set.contains(root.val)){
+            return root;
+        }
 
-class Path {
-    TreeNode node;
-    String path;
+        TreeNode left = dfs(root.left, set);
+        TreeNode right = dfs(root.right, set);
 
-    Path(TreeNode node, String path) {
-        this.node = node;
-        this.path = path;
-    }
+        if(left == null){
+            return right;
+        }
+        if(right == null){
+            return left;
+        }
 
-    Path() {
-
+        return root;
     }
 }
